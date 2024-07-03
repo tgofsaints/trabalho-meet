ac4ec9393116cb5b7da26dd16641b93ab7f649ea
Thiago Gonzaga dos Santos <thiagogsantos2013@gmail.com>
2024-07-03 20:55:02 -0300

    projeto final da disciplina

--- server.js
+++ server.js
@@ -1,29 +1,30 @@
-const express = require('express')
-const app = express()
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
-const { v4: uuidV4 } = require('uuid')
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const { v4: uuidV4 } = require('uuid');
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 app.get('/', (req, res) => {
-  res.redirect(`/${uuidV4()}`)
-})
+  res.render('login'); 
+});
 
+app.get('/generate-room', (req, res) => {
+  res.redirect(`/${uuidV4()}`);
+});
 app.get('/:room', (req, res) => {
-  res.render('room', { roomId: req.params.room })
-})
-
+  res.render('room', { roomId: req.params.room });
+});
 io.on('connection', socket => {
   socket.on('join-room', (roomId, userId) => {
-    socket.join(roomId)
-    socket.to(roomId).emit('user-connected', userId)
+    socket.join(roomId);
+    socket.to(roomId).emit('user-connected', userId);
 
     socket.on('disconnect', () => {
-      socket.to(roomId).emit('user-disconnected', userId)
-    })
-  })
-})
+      socket.to(roomId).emit('user-disconnected', userId);
+    });
+  });
+});
 
-server.listen(3000)
+server.listen(3000);
